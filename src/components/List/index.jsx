579875708418ec186a7af0a13ead4fcc69890554
Danius Vilimas <danvil97@yandex.ru>
2020-05-14 05:18:35 +0300

    json-server added, add/remove list, display tasks for list

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import classNames from 'classnames';
-import Badge from '../Badge';
+import axios from 'axios';
 
 import removeSvg from '../../assets/img/remove.svg';
 
+import Badge from '../Badge';
 import './List.scss';
 
 const List = ({ items, isRemovable, onClick, onRemove }) => {
   const removeList = (item) => {
     if (window.confirm('Вы действительно хотите удалить список?')) {
-      onRemove(item);
+      axios.delete('http://localhost:3001/lists/' + item.id).then(() => {
+        onRemove(item.id);
+      });
     }
   };
   return (
@@ -19,7 +22,7 @@ const List = ({ items, isRemovable, onClick, onRemove }) => {
           key={index}
           className={classNames(item.className, { active: item.active })}
         >
-          <i>{item.icon ? item.icon : <Badge color={item.color} />}</i>
+          <i>{item.icon ? item.icon : <Badge color={item.color.name} />}</i>
           <span>{item.name}</span>
           {isRemovable && (
             <img
